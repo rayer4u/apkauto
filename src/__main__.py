ee239248809b8a555a83db485782f1557aac23a2
roybi <rayer4u@gmail.com>
2015-06-02 14:40:54 +0800

    windows版本label乱码问题

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 
 import sys
 from apk_auto import apk_auto
@@ -7,12 +8,22 @@ if __name__ == "__main__":
     label = ""
     if len(sys.argv) > 1:
         label = sys.argv[1]
-        if sys.stdin.encoding.lower() != 'utf-8':
-            label = label.decode(sys.stdin.encoding).encode('utf-8')
     if label == "":
         print("请输入标签（无标签的包可能被删除，无需标签直接按回车）：".decode('utf-8'))
         label = raw_input()
 
-    apk_auto(label)
+    #label转码成utf8
+    if sys.version_info[0] > 2:
+        tp = str
+    else:
+        tp = unicode
+    if isinstance(label, tp):
+        label = label.encode('utf-8')
+    else:
+        if sys.stdin.encoding.lower() != 'utf-8':
+            label = label.decode(sys.stdin.encoding).encode('utf-8')
+
+    print(label)
+    #apk_auto(label)
     print("结束，按回车退出。".decode('utf-8'))
     raw_input()
